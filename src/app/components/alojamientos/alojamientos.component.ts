c38dc215d4bc8ee690eef2c6a704bf64de499550
Alvizo07 <al291339@edu.uaa.mx>
2023-05-13 13:12:51 -0600

    Cambios para el githubpages(final)

--- src/app/components/alojamientos/alojamientos.component.ts
+++ src/app/components/alojamientos/alojamientos.component.ts
@@ -16,7 +16,7 @@ export class AlojamientosComponent {
       description:
         'Disfruta de unas lindas vacaciones en este cómodo alojamiento, a solo unos pasos del mar, disfrutaras las soleadas tardes de Cancún en sus lindas albercas.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento1.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento1.jpg',
     },
     {
       id: 1,
@@ -24,7 +24,7 @@ export class AlojamientosComponent {
       description:
         'Esta unidad lujosamente equipada está equipada con una increíble terraza de 650 pies cuadrados con vista al mar, cocina de mármol, sistema de wifi de malla fuerte.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento2.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento2.jpg',
     },
     {
       id: 2,
@@ -32,7 +32,7 @@ export class AlojamientosComponent {
       description:
         'Increíble villa estilo A-frame, disfruta del bosque con este estilo que de cabaña que a todo el mundo le encanta, le nombramos chalet Mágica ya que  iluminamos .',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento3.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento3.jpg',
     },
     {
       id: 3,
@@ -40,7 +40,7 @@ export class AlojamientosComponent {
       description:
         'Una de las joyas de la corona de Corales, la exclusiva comunidad dentro de Puntacana Resort & Club. Corales 83 cuenta con un increíble patio interior, una decoración única.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento4.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento4.jpg',
     },
     {
       id: 4,
@@ -48,7 +48,7 @@ export class AlojamientosComponent {
       description:
         'A solo 10 minutos de la Plaza Cataluña. El aparthotel cuenta con estudios amplios y atractivos que permiten disfrutar de una estancia más cómoda y agradable. Excelente conexion WiFi ',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento5.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento5.jpg',
     },
     {
       id: 5,
@@ -56,7 +56,7 @@ export class AlojamientosComponent {
       description:
         'Casa ecológica. 2 km de Restuarant Qooqut nuan. 2 habitaciones con camas dobles y colchones que se pueden poner en la sala de estar. Con electricidad usando el generador.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento6.jpeg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento6.jpeg',
     },
     {
       id: 6,
@@ -64,7 +64,7 @@ export class AlojamientosComponent {
       description:
         'se encuentra en el centro de Oaxaca y ofrece piscina al aire libre, instalaciones para no fumadores con zonas designadas para fumadores, restaurante y conexión Wi-Fi gratuita.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento7.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento7.jpg',
     },
     {
       id: 7,
@@ -72,7 +72,7 @@ export class AlojamientosComponent {
       description:
         'Con una de las playas más destacadas del mundo, Island Resort te da la bienvenida para disfrutar de unas vacaciones originales llenas de playa. Villa de agua entera en zanco con jacuzzi.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento8.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento8.jpg',
     },
     {
       id: 8,
@@ -80,7 +80,7 @@ export class AlojamientosComponent {
       description:
         'El Hard Rock se encuentra en Mazatlán, a pocos pasos de la playa de Camaron, y ofrece alojamiento con piscina al aire libre, aparcamiento privado gratuito. Este hotel cuenta con 5 estrellas.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento9.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento9.jpg',
     },
     {
       id: 9,
@@ -88,7 +88,7 @@ export class AlojamientosComponent {
       description:
         'El América Bittar Hotel se encuentra a 400 metros de la torre de televisión de Brasilia y a 1,3 km del Complejo Cultural de la República, y ofrece habitaciones modernas y un restaurante.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento10.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento10.jpg',
     },
     {
       id: 10,
@@ -96,7 +96,7 @@ export class AlojamientosComponent {
       description:
         '20 años de experiencia en el desierto del Sahara con nuestro MohammedMutlakcamp. Somos el campamento de SUPERHOSTS, en el área protegida más profunda. Perfecta para despejarte.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento11.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento11.jpg',
     },
     {
       id: 11,
@@ -104,7 +104,7 @@ export class AlojamientosComponent {
       description:
         'Si buscas un lugar hogareño para tu estadía, esta habitación en nuestra casa familiar es perfecta para ti. Te ofrecemos todas las comodidades y una experiencia auténtica de la vida en la ciudad.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento12.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento12.jpg',
     },
   ];
 
--- src/app/shared/lugares.service.ts
+++ src/app/shared/lugares.service.ts
@@ -12,7 +12,7 @@ export class LugaresService {
       description:
         'Disfruta de unas lindas vacaciones en este cómodo alojamiento, a solo unos pasos del mar, disfrutaras las soleadas tardes de Cancún en sus lindas albercas.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento1.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento1.jpg',
     },
     {
       id: 1,
@@ -20,7 +20,7 @@ export class LugaresService {
       description:
         'Esta unidad lujosamente equipada está equipada con una increíble terraza de 650 pies cuadrados con vista al mar, cocina de mármol, sistema de wifi de malla fuerte.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento2.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento2.jpg',
     },
     {
       id: 2,
@@ -28,7 +28,7 @@ export class LugaresService {
       description:
         'Increíble villa estilo A-frame, disfruta del bosque con este estilo que de cabaña que a todo el mundo le encanta, le nombramos chalet Mágica ya que  iluminamos .',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento3.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento3.jpg',
     },
     {
       id: 3,
@@ -36,7 +36,7 @@ export class LugaresService {
       description:
         'Una de las joyas de la corona de Corales, la exclusiva comunidad dentro de Puntacana Resort & Club. Corales 83 cuenta con un increíble patio interior, una decoración única.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento4.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento4.jpg',
     },
     {
       id: 4,
@@ -44,7 +44,7 @@ export class LugaresService {
       description:
         'A solo 10 minutos de la Plaza Cataluña. El aparthotel cuenta con estudios amplios y atractivos que permiten disfrutar de una estancia más cómoda y agradable. Excelente conexion WiFi ',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento5.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento5.jpg',
     },
     {
       id: 5,
@@ -52,7 +52,7 @@ export class LugaresService {
       description:
         'Casa ecológica. 2 km de Restuarant Qooqut nuan. 2 habitaciones con camas dobles y colchones que se pueden poner en la sala de estar. Con electricidad usando el generador.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento6.jpeg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento6.jpeg',
     },
     {
       id: 6,
@@ -60,7 +60,7 @@ export class LugaresService {
       description:
         'se encuentra en el centro de Oaxaca y ofrece piscina al aire libre, instalaciones para no fumadores con zonas designadas para fumadores, restaurante y conexión Wi-Fi gratuita.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento7.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento7.jpg',
     },
     {
       id: 7,
@@ -68,7 +68,7 @@ export class LugaresService {
       description:
         'Con una de las playas más destacadas del mundo, Island Resort te da la bienvenida para disfrutar de unas vacaciones originales llenas de playa. Villa de agua entera en zanco con jacuzzi.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento8.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento8.jpg',
     },
     {
       id: 8,
@@ -76,7 +76,7 @@ export class LugaresService {
       description:
         'El Hard Rock se encuentra en Mazatlán, a pocos pasos de la playa de Camaron, y ofrece alojamiento con piscina al aire libre, aparcamiento privado gratuito. Este hotel cuenta con 5 estrellas.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento9.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento9.jpg',
     },
     {
       id: 9,
@@ -84,7 +84,7 @@ export class LugaresService {
       description:
         'El América Bittar Hotel se encuentra a 400 metros de la torre de televisión de Brasilia y a 1,3 km del Complejo Cultural de la República, y ofrece habitaciones modernas y un restaurante.',
       price: 100,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento10.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento10.jpg',
     },
     {
       id: 10,
@@ -92,7 +92,7 @@ export class LugaresService {
       description:
         '20 años de experiencia en el desierto del Sahara con nuestro MohammedMutlakcamp. Somos el campamento de SUPERHOSTS, en el área protegida más profunda. Perfecta para despejarte.',
       price: 200,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento11.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento11.jpg',
     },
     {
       id: 11,
@@ -100,7 +100,7 @@ export class LugaresService {
       description:
         'Si buscas un lugar hogareño para tu estadía, esta habitación en nuestra casa familiar es perfecta para ti. Te ofrecemos todas las comodidades y una experiencia auténtica de la vida en la ciudad.',
       price: 50,
-      imageUrl: '../assets/imagenesAlojamientos/alojamiento12.jpg',
+      imageUrl: 'assets/imagenesAlojamientos/alojamiento12.jpg',
     },
   ];
   constructor() { 
